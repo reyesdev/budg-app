5b42ac4cdfda0a72cc8d1940ce02c6c2c041c241
richard reyes <richardreyes.ca@gmail.com>
2019-06-27 22:45:01 -0400

    - app logic

--- src/App.js
+++ src/App.js
@@ -1,92 +1,42 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import React, { Component } from 'react';
+import './App.css';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  button: {
-    margin: theme.spacing(1),
-  },
-  buttonLarge: {
-    margin: theme.spacing(1),
-    fontSize: '50px',
-    padding: '0 50px',
-  },
-  input: {
-    display: 'none',
-  },
-}));
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      budget: 600,
+      numArr: [1,5,10,20,50],
+      btnMinus: []
+    };
+  }
+  handleMinus = (value) => {
+    let newvalue = this.state.budget - value;
+    this.setState({budget: newvalue});
+  }
+  handleAdd = (value) => {
+    let newvalue = this.state.budget + value;
+    this.setState({budget: newvalue});
+  }
+  render() {
+    const numArr = [1,5,10,20,50];
+    let buttonsMinus = [];
+    let buttonsAdd = [];
 
-export default function App() {
-    const classes = useStyles();
-
+    // ? Create buttons
+    for (let i of numArr) {
+      buttonsMinus.push(<button key={i} onClick={() => this.handleMinus(i)}>-&nbsp;{i}</button>);
+      buttonsAdd.push(<button key={i} onClick={() => this.handleAdd(i)}>+&nbsp;{i}</button>)
+    }
     return (
-      <Container maxWidth="sm" className={classes.container}>
-        <div className={classes.root}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                600
-              </Paper>
-            </Grid>
-            <Grid item xs={8}>
-              <Paper className={classes.paper}>
-                <Button size="large" variant="contained" color="primary" className={classes.buttonLarge}>
-                  - 1
-                </Button>
-                <Button size="large" variant="contained" color="primary" className={classes.buttonLarge}>
-                  -5
-                </Button>
-                <Button size="large" variant="contained" color="primary" className={classes.buttonLarge}>
-                  -10
-                </Button>
-                <Button size="large" variant="contained" color="primary" className={classes.buttonLarge}>
-                  -20
-                </Button>
-                <Button size="large" variant="contained" color="primary" className={classes.buttonLarge}>
-                  -50
-                </Button>
-                
-              </Paper>
-            </Grid>
-            <Grid item xs={4}>
-              <Paper className={classes.paper}>
-                <Button variant="contained" color="primary" className={classes.button}>
-                    +1
-                </Button>
-                <Button variant="contained" color="primary" className={classes.button}>
-                  +5
-                </Button>
-                <Button variant="contained" color="primary" className={classes.button}>
-                  +10
-                </Button>
-                <Button variant="contained" color="primary" className={classes.button}>
-                  +20
-                </Button>
-                <Button variant="contained" color="primary" className={classes.button}>
-                  +50
-                </Button>
-              </Paper>
-            </Grid>
-          </Grid>
-        </div>
-      </Container>
+      <React.Fragment>
+        {this.state.budget}<br />
+        {buttonsMinus}<br />
+        {buttonsAdd}
+      </React.Fragment>
     );
   }
+}
 
-
-
+export default App;
+  
