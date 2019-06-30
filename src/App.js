3fe5cb1bc11c1db7908be37239d154d12f2c51cb
richard reyes <richardreyes.ca@gmail.com>
2019-06-30 16:10:08 -0400

    - setting to update budget value in place

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super(props);
     this.state = {
       budget: 600,
+      value: '',
       numArr: [1,5,10,20,50],
       mobileNav: false
     };
@@ -37,6 +38,14 @@ class App extends Component {
     this.setState({budget: 600});
     localStorage.setItem('budgetstore', this.state.budget);
   }
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit = (event) => {
+    this.setState({budget: this.state.value});
+    localStorage.setItem('budgetstore', this.state.value);
+    event.preventDefault();
+  }
 
   render() {
     // const numArr = [1,5,10,20,50];
@@ -53,11 +62,6 @@ class App extends Component {
       return (
         <React.Fragment>
           <div className="container-fluid">
-            <div className="row mb-0">
-              <div className="col-12">
-                <h1>Budget</h1>
-              </div>
-            </div>
             <div className="row">
               <div className="col-12">
                 <h2 className="display-4">
@@ -89,6 +93,12 @@ class App extends Component {
           <div className="row mb-0">
             <div className="col-12">
               <h2>Settings</h2>
+              <form key={this.state.value.toString()} onSubmit={this.handleSubmit}>
+                <h3>Update budget.</h3>
+                <p>Manually update your budget below:</p>
+                <input  type='text' value={this.state.value} onChange={this.handleChange} />
+                <input type="submit" value="Submit" />
+              </form>
             </div>
           </div>
         </div>
